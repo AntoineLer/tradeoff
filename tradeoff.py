9171a2faa3edf00a4a0d163b9ccfbcc7dd76b4c6
AntoineLer <antoine.leroy@hotmail.com>
2019-11-17 00:55:07 +0100

    irrelevant variables mais pas sur

--- tradeoff.py
+++ tradeoff.py
@@ -20,9 +20,9 @@ def make_data(n_samples, n_sets, random_state=None):
     y_test = function + 0.1*random.normal(0, 1, n_samples)
     return (x_train, y_train, x_test, y_test)
 
-def linear_model(x_train, y_train, x_test, y_test, residual_error):
-    linear_estimators = [LinearRegression().fit(x.reshape(-1, 1), y) for x, y in zip(x_train, y_train)]
-    linear_prediction = [estimator.predict(x_test.reshape(-1, 1)) for estimator in linear_estimators]
+def linear_model(x_train, y_train, x_test, residual_error, number_irrelevant_variables):
+    linear_estimators = [LinearRegression().fit(add_irrelevant_variables(x, number_irrelevant_variables), y) for x, y in zip(x_train, y_train)]
+    linear_prediction = [estimator.predict(add_irrelevant_variables(x_test, number_irrelevant_variables)) for estimator in linear_estimators]
 
     noise = np.full(x_train.shape[1], residual_error)
     squared_bias = (f(x_test) - np.mean(linear_prediction, 0))**2
@@ -39,9 +39,9 @@ def linear_model(x_train, y_train, x_test, y_test, residual_error):
     plt.legend()
     plt.savefig("tradeoff_Q_d_linear.pdf")
 
-def non_linear_model(x_train, y_train, x_test, y_test, residual_error):
-    linear_estimators = [KNeighborsRegressor(n_neighbors=75).fit(x.reshape(-1, 1), y) for x, y in zip(x_train, y_train)]
-    linear_prediction = [estimator.predict(x_test.reshape(-1, 1)) for estimator in linear_estimators]
+def non_linear_model(x_train, y_train, x_test, residual_error, number_irrelevant_variables):
+    linear_estimators = [KNeighborsRegressor(n_neighbors=100).fit(add_irrelevant_variables(x, number_irrelevant_variables), y) for x, y in zip(x_train, y_train)]
+    linear_prediction = [estimator.predict(add_irrelevant_variables(x_test, number_irrelevant_variables)) for estimator in linear_estimators]
 
     noise = np.full(x_train.shape[1], residual_error)
     squared_bias = (f(x_test) - np.mean(linear_prediction, 0))**2
@@ -66,11 +66,21 @@ def noise(x, y):
             residual_error.append((y[i][k] - f(x[i][k]))**2)
     return np.mean(np.array(residual_error))
 
+def add_irrelevant_variables(X, number_irrelevant_variables, random_state=None):
+    if number_irrelevant_variables == 0:
+        return X.reshape(-1, 1)
+    random = check_random_state(random_state)
+    variables = np.zeros((X.shape[0], 1 + number_irrelevant_variables))
+    for i in range(X.shape[0]):
+        variables[i] = np.concatenate(([X[i]], random.uniform(-10, 10, number_irrelevant_variables)))
+    return variables.reshape(-1, 1 + number_irrelevant_variables)
+
 if __name__ == "__main__":
     n_samples = 1000
     n_sets = 50
+    number_irrelevant_variables = 0
     (x_train, y_train, x_test, y_test) = make_data(n_samples, n_sets)
     residual_error = noise(x_train, y_train)
 
-    linear_model(x_train, y_train, x_test, y_test, residual_error)
-    non_linear_model(x_train, y_train, x_test, y_test, residual_error)
+    linear_model(x_train, y_train, x_test, residual_error, number_irrelevant_variables)
+    non_linear_model(x_train, y_train, x_test, residual_error, number_irrelevant_variables)
